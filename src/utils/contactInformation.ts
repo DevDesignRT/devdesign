9dd49fafbf7079014339804f4fc85268b1a54960
Roman Tuomisto <romantuomisto@gmail.com>
2021-08-28 12:41:37 +0300

    adds team membe joseph

--- src/utils/contactInformation.ts
+++ src/utils/contactInformation.ts
@@ -17,18 +17,32 @@ export const ContactInformation: IListItem[] = [
   },
 ];
 
-export const Avatar: IPictureProps = {
-  alt: "Avatar of: Roman Tuomisto - Software Engineer",
-  src:
-    "https://res.cloudinary.com/romantuomisto-cloud/image/upload/v1623748157/YelpCamp/roman-dev.png",
-  sources: [
-    {
-      srcSet:
-        "https://res.cloudinary.com/romantuomisto-cloud/image/upload/v1623748157/YelpCamp/roman-dev.png",
-      media: "(min-width:650px)",
-    },
-  ],
-};
+export const Avatar: IPictureProps[] = [
+  {
+    alt: "Avatar of: Roman Tuomisto - Senior FullStack Web Developer",
+    src:
+      "https://res.cloudinary.com/romantuomisto-cloud/image/upload/v1623748157/YelpCamp/roman-dev.png",
+    sources: [
+      {
+        srcSet:
+          "https://res.cloudinary.com/romantuomisto-cloud/image/upload/v1623748157/YelpCamp/roman-dev.png",
+        media: "(min-width:650px)",
+      },
+    ],
+  },
+  {
+    alt: "Avatar of: Joseph Heidari - FullStack Web Develpoer and DevOps",
+    src:
+      "https://res.cloudinary.com/romantuomisto-cloud/image/upload/v1630143572/YelpCamp/joseph-heidari.jpg",
+    sources: [
+      {
+        srcSet:
+          "https://res.cloudinary.com/romantuomisto-cloud/image/upload/v1630143572/YelpCamp/joseph-heidari.jpg",
+        media: "(min-width:650px)",
+      },
+    ],
+  },
+];
 
 export const CompanyInformation: IListItem[] = [
   {
